15e59de50936937cadf9e089708410b964306962
Mari-marina <shunkina9292@mail.ru>
2021-12-04 13:23:18 +0300

    1

--- funtions.php
+++ funtions.php
@@ -1,28 +1,12 @@
 <?php
 
-define('THEME_ROOT', get_template_directory_uri());
-	define('CSS_DIR', THEME_ROOT . '/assets/css');
-	define('IMG_DIR', THEME_ROOT . '/assets/images');
+add_action( 'wp_enqueue_scripts', 'arch_style' );
+add_action( 'wp_enqueue_scripts', 'arch_scripts' );
 
-
-add_action( 'wp_enqueue_scripts', 'adore_style' );
-add_action( 'wp_enqueue_scripts', 'adore_scripts' );
-add_action( 'after_setup_theme', 'theme_register_nav_menu' );
-
-
-function theme_register_nav_menu() {
-	register_nav_menu( 'footer_menu', 'Меню в подвале');
-	
-}
-
-function adore_style() {
-	
+function arch_style() {	
 	wp_enqueue_style( 'main-style', get_stylesheet_uri() );
-
-
 }
 
-function adore_scripts() {
-	
-wp_enqueue_script( 'script-adore', get_template_directory_uri() .'/assets/js/scripts.js', array(), '1.0', true );
+function arch_scripts() {	
+wp_enqueue_script( 'script-adore', get_template_directory_uri() .'/assets/js/scripts.js', );
 }
--- header.php
+++ header.php
@@ -12,20 +12,39 @@
 </head>
 
 <body>    
-    <header class="header">
-        <div class="header__wrapper container">          
-                <a class="header__logo logo" href="<?php bloginfo('url'); ?>">
-                    <img class="logo__img" src="<?php echo IMG_DIR; ?>/header/header-logo.svg" alt="logo">
-                </a>     
-
-                <ul class="header__contacts">
-                    
-                    <li class="header__item">
-                    <a href="https://t.me/adore_women
-                        " class="header__link">
-                            <img class="header__img" src="<?php echo IMG_DIR; ?>/header/Telegram.svg" alt="Telegram">
-                        </a>
-                    </li>
-                </ul>                
-            </div>         
-</header>
+    <Header class="header">
+        <nav class="header__nav">
+            <div class="header__wrapper">
+                <img class="header__logo logo" src="./assets/images/logo.svg" alt="">
+                <ul class="header__list">
+                    <li class="header__item"> <a class="header__link" href="#">Design Gallery</a> </li>
+                    <li class="header__item"><a class="header__link" href="#">List of Architect</a></li>
+                    <li class="header__item"><a class="header__link" href="#">Articles</a></li>
+                    <li class="header__item"><a class="header__link" href="#">How it Works</a></li>
+                </ul>
+            </div>
+            <div class="header__wrap">
+                <button class="header__btn header__btn--search"><img src="./assets/images/Search.svg" alt=""></button>
+                <button class="header__btn header__btn--left">Sign In</button>
+                <button class="header__btn header__btn--right">Sign Up</button>
+            </div>
+        </nav>
+        <div class="burger"></div>
+        <div class="container">
+            <main class="main">
+                <h1 class="main__title">Build Your <br>
+                    Dream <span class="main__title--orange">House</span> </h1>
+                <div class="main__wrapper">
+                    <div class="main__wrap">
+                        <p class="main__text">The first and most trusted marketplace of design & build house in The
+                            World.
+                            100% guaranteed Build safe, comfortable and transparent with a project management for the
+                            best
+                            result</p>
+                        <button class="main__btn">Free Consultation</button>
+                    </div>
+                    <img class="main__img" src="./assets/images/main.png" alt="">
+                </div>
+            </main>
+        </div>
+    </Header>
